5f6e589e62847bd489f54b750862cbed697b2dab
Thanga Ayyanar <gldayan8@gmail.com>
2021-10-05 16:06:58 +0530

    KBTDD: The Root of All Evil
    I have done the changes required for this in previous commit itself :)

--- KBTDD/Tests/KBTDDTests/KBTDDTests.swift
+++ KBTDD/Tests/KBTDDTests/KBTDDTests.swift
@@ -7,24 +7,12 @@ final class KBTDDTests: XCTestCase {
 
         let fiveDollar = Money.dollar(amount: 5)
         XCTAssertEqual(fiveDollar.times(2), Money.dollar(amount: 10))
-        print("Log: \(fiveDollar.times(2).amount) \(Money.dollar(amount: 10).amount)")
         XCTAssertEqual(fiveDollar.times(3), Money.dollar(amount: 15))
-    }
-
-    func testFrancMultiplication() {
-
-        let fiveFranc = Money.franc(amount: 5)
-        XCTAssertEqual(fiveFranc.times(2), Money.franc(amount: 10))
-        XCTAssertEqual(fiveFranc.times(3), Money.franc(amount: 15))
     }
 
     func testEquality() {
         XCTAssertTrue(Money.dollar(amount: 15) == Money.dollar(amount:15))
         XCTAssertFalse(Money.dollar(amount: 15) == Money.dollar(amount:18))
-
-        XCTAssertTrue(Money.franc(amount: 15) == Money.franc(amount:15))
-        XCTAssertFalse(Money.franc(amount: 15) == Money.franc(amount:18))
-
         XCTAssertFalse(Money.dollar(amount: 15) == Money.franc(amount:15))
     }
 
