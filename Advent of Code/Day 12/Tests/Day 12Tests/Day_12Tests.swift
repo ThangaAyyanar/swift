36afd8462980e300619cadb181e4caee091a4b9c
Thanga Ayyanar <gldayan8@gmail.com>
2022-01-29 22:33:00 +0530

    AOC: Day 12 input file parsing changes

--- Advent of Code/Day 12/Tests/Day 12Tests/Day_12Tests.swift
+++ Advent of Code/Day 12/Tests/Day 12Tests/Day_12Tests.swift
@@ -8,14 +8,31 @@ final class Day_12Tests: XCTestCase {
         let contents = try! String(contentsOf: fileURL, encoding: .utf8)
         let allInputs = contents.components(separatedBy: .newlines)
 
-        var new_inputs = [String:String]()
+        var new_inputs = [String:[String]]()
 
         for input in allInputs {
-            let splited_string = input.split(separator: " - ")
+            let splitted_string = input.split(separator: "-")
+            if splitted_string.count == 2 {
+
+                if let old_value = new_inputs[String(splitted_string[0])] {
+                    new_inputs[String(splitted_string[0])]!.append(String(splitted_string[1]))
+                }
+                else {
+                    new_inputs[String(splitted_string[0])] = [String(splitted_string[1])]
+                }
+
+                if let old_value = new_inputs[String(splitted_string[1])] {
+                    new_inputs[String(splitted_string[1])]!.append(String(splitted_string[0]))
+                }
+                else {
+                    new_inputs[String(splitted_string[1])] = [String(splitted_string[0])]
+                }
+            }
         }
-        let day12 = Day_12(inputs: new_inputs)
-        let result = day12.part1()
-        print(result)
+        print(new_inputs)
+        /*let day12 = Day_12(inputs: new_inputs)*/
+        /*let result = day12.part1()*/
+        /*print(result)*/
     }
 
 }
