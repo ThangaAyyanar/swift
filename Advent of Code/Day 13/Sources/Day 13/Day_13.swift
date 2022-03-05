7bc814a6f78289c4ef1c322d44f83f4a7f1318b1
Thanga Ayyanar <gldayan8@gmail.com>
2022-03-05 12:01:25 +0530

    AOC: Day 13 completion

--- Advent of Code/Day 13/Sources/Day 13/Day_13.swift
+++ Advent of Code/Day 13/Sources/Day 13/Day_13.swift
@@ -1,10 +1,14 @@
-public struct Position{
+public struct Position: Equatable{
     let x: Int
     let y: Int
     
     init(_ x: Int, _ y: Int) {
         self.x = x
         self.y = y
+    }
+
+    public static func == (lhs: Position, rhs: Position) -> Bool {
+        return lhs.x == rhs.x && lhs.y == rhs.y
     }
 }
 
@@ -14,9 +18,162 @@ public enum Folding {
 }
 
 
-public struct Day_13 {
-    public private(set) var text = "Hello, World!"
+public class Day_13 {
 
-    public init() {
+    let folding: [Folding]
+    let allPoints: [Position]
+    let maxX: Int
+    let maxY: Int
+
+    public init(folding: [Folding], allPoints: [Position], maxX: Int, maxY: Int) {
+        self.folding = folding
+        self.allPoints = allPoints
+        self.maxX = maxX
+        self.maxY = maxY
     }
+
+
+    func part1() {
+
+        var newPoints = allPoints
+
+        let fold = folding.first!
+
+        var tempPoints = [Position]()
+
+        switch fold {
+        case .x(let value):
+            for position in newPoints {
+                let x = position.x
+                if x > value {
+                    let newX = value - (position.x - value)
+                    tempPoints.append(Position(newX, position.y))
+                }
+                else {
+                    tempPoints.append(position)
+                }
+            }
+
+        case .y(let value):
+            for position in newPoints {
+                let y = position.y
+                if y > value {
+                    let newY = value - (position.y - value)
+                    tempPoints.append(Position(position.x, newY))
+                }
+                else {
+                    tempPoints.append(position)
+                }
+            }
+
+        }
+        newPoints = tempPoints
+    
+
+        var uniquePoints = [Position]()
+        for point in newPoints {
+            if !uniquePoints.contains(point) {
+                uniquePoints.append(point)
+            }
+        }
+        print(uniquePoints.count)
+
+    }
+
+    func part2() {
+
+        var newPoints = allPoints
+
+        for fold in folding {
+
+            var tempPoints = [Position]()
+
+            switch fold {
+            case .x(let value):
+                for position in newPoints {
+                    let x = position.x
+                    if x > value {
+                        let newX = value - (position.x - value)
+                        tempPoints.append(Position(newX, position.y))
+                    }
+                    else {
+                        tempPoints.append(position)
+                    }
+                }
+            case .y(let value):
+                for position in newPoints {
+                    let y = position.y
+                    if y > value {
+                        let newY = value - (position.y - value)
+                        tempPoints.append(Position(position.x, newY))
+                    }
+                    else {
+                        tempPoints.append(position)
+                    }
+                }
+            }
+            newPoints = tempPoints
+        }
+        print(newPoints)
+
+    }
+
+    // Understand the problem
+
+    /*func display(grid: [[Int]]) {*/
+
+        /*for values in grid {*/
+            /*for value in values {*/
+                /*print("\(value)", terminator: "")*/
+            /*}*/
+            /*print("")*/
+        /*}*/
+    /*}*/
+
+    /*func part1_new() {*/
+
+        /*var grid = Array(repeating: Array(repeating: 0, count: maxX+1), count: maxY+1)*/
+
+        /*for position in allPoints{*/
+            /*grid[position.y][position.x] = 1*/
+        /*}*/
+
+        /*var newPoints = allPoints*/
+
+        /*for fold in folding {*/
+            /*var tempPoints = [Position]()*/
+            /*if case Folding.x(let value) = fold {*/
+                /*for position in newPoints {*/
+                    /*let x = position.x*/
+                    /*if x > value {*/
+                        /*let newX = maxX - x*/
+                        /*grid[position.y][newX] = 1*/
+                        /*grid[position.y][position.x] = 0*/
+                        /*tempPoints.append(Position(newX, position.y))*/
+                    /*}*/
+                    /*else {*/
+                        /*tempPoints.append(position)*/
+                    /*}*/
+                /*}*/
+                /*newPoints = tempPoints*/
+            /*}*/
+            /*else if case Folding.y(let value) = fold {*/
+                /*for position in newPoints {*/
+                    /*let y = position.y*/
+                    /*if y > value {*/
+                        /*let newY = maxY - y*/
+                        /*grid[newY][position.x] = 1*/
+                        /*grid[position.y][position.x] = 0*/
+                        /*tempPoints.append(Position(position.x, newY))*/
+                    /*}*/
+                    /*else {*/
+                        /*tempPoints.append(position)*/
+                    /*}*/
+                /*}*/
+                /*newPoints = tempPoints*/
+            /*}*/
+        /*}*/
+
+    /*}*/
+
 }
--- Advent of Code/Day 13/Tests/Day 13Tests/Day_13Tests.swift
+++ Advent of Code/Day 13/Tests/Day 13Tests/Day_13Tests.swift
@@ -3,12 +3,7 @@ import XCTest
 
 final class Day_13Tests: XCTestCase {
 
-    func test_part1_sample() {
-
-        let fileURL = Bundle.module.url(forResource: "test", withExtension: "txt")!
-        let contents = try! String(contentsOf: fileURL, encoding: .utf8)
-        let allInputs = contents.components(separatedBy: .newlines)
-
+    func process_inputs(allInputs: [String]) -> (allPoints: [Position], folding: [Folding], maxX: Int, maxY: Int) {
         var allPoints = [Position]()
         var folding = [Folding]()
         var maxX = 0
@@ -21,18 +16,15 @@ final class Day_13Tests: XCTestCase {
             else if input.hasPrefix("fold along y=") {
 
                 let points = input.split(separator: "=")
-                print(points)
                 folding.append(Folding.y(Int(points[1])!))
             }
             else if input.hasPrefix("fold along x=") {
 
                 let points = input.split(separator: "=")
-                print(points)
                 folding.append(Folding.x(Int(points[1])!))
             }
             else {
                 let points = input.split(separator: ",")
-                print(points)
                 let x = Int(points[0])!
                 let y = Int(points[1])!
                 allPoints.append(Position(x, y))
@@ -44,11 +36,59 @@ final class Day_13Tests: XCTestCase {
                 }
             }
         }
+        return (allPoints: allPoints, folding: folding, maxX: maxX, maxY: maxY)
+    }
 
+    func test_part1_sample() {
 
-        print(allPoints)
-        print(folding)
-        print(maxX)
-        print(maxY)
+        let fileURL = Bundle.module.url(forResource: "test", withExtension: "txt")!
+        let contents = try! String(contentsOf: fileURL, encoding: .utf8)
+        let allInputs = contents.components(separatedBy: .newlines)
+
+        let processed_inputs = process_inputs(allInputs: allInputs)
+
+        let day13 = Day_13(folding: processed_inputs.folding, allPoints: processed_inputs.allPoints, maxX: processed_inputs.maxX, maxY:processed_inputs.maxY)
+        day13.part1()
+
+    }
+
+    func test_part1() {
+
+        let fileURL = Bundle.module.url(forResource: "input", withExtension: "txt")!
+        let contents = try! String(contentsOf: fileURL, encoding: .utf8)
+        let allInputs = contents.components(separatedBy: .newlines)
+
+        let processed_inputs = process_inputs(allInputs: allInputs)
+
+        let day13 = Day_13(folding: processed_inputs.folding, allPoints: processed_inputs.allPoints, maxX: processed_inputs.maxX, maxY:processed_inputs.maxY)
+        day13.part1()
+
+    }
+
+    func test_part2_sample() {
+
+        let fileURL = Bundle.module.url(forResource: "test", withExtension: "txt")!
+        let contents = try! String(contentsOf: fileURL, encoding: .utf8)
+        let allInputs = contents.components(separatedBy: .newlines)
+
+        let processed_inputs = process_inputs(allInputs: allInputs)
+
+        let day13 = Day_13(folding: processed_inputs.folding, allPoints: processed_inputs.allPoints, maxX: processed_inputs.maxX, maxY:processed_inputs.maxY)
+        day13.part2()
+
+    }
+
+    func test_part2() {
+
+        let fileURL = Bundle.module.url(forResource: "input", withExtension: "txt")!
+        let contents = try! String(contentsOf: fileURL, encoding: .utf8)
+        let allInputs = contents.components(separatedBy: .newlines)
+
+        let processed_inputs = process_inputs(allInputs: allInputs)
+
+        let day13 = Day_13(folding: processed_inputs.folding, allPoints: processed_inputs.allPoints, maxX: processed_inputs.maxX, maxY:processed_inputs.maxY)
+        day13.part2()
+        // plot the point in Desmos and found the answer
+        //ALREKFKU
     }
 }
